098b69768c0d9664912e366a7a14d2255a89c7a1
Cidospark <cidospark27@gmail.com>
2023-08-31 11:06:36 +0100

    Initial commit

---
+++ DevsTutorialCenterMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevsTutorialCenterMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
