68eb82c29dc4c4ef7b1033f798ba0a82a8cf643c
Francis Ibe <49306271+Cidospark@users.noreply.github.com>
2023-09-02 18:11:19 +0100

    few refactoring

---
+++ DevsTutorialCenterMVC/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevsTutorialCenterMVC.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
---
+++ DevsTutorialCenterMVC/Controllers/EditorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevsTutorialCenterMVC.Controllers
+{
+    public class EditorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
