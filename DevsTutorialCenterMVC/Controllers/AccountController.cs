17672b6c304fa2ca6796451540d1a445d530d605
Bentley-Enuvi <bentleyenuvi@gmail.com>
2023-09-17 21:57:20 +0100

    Update

--- DevsTutorialCenterMVC/Controllers/AccountController.cs
+++ DevsTutorialCenterMVC/Controllers/AccountController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevsTutorialCenterMVC.Data.Entities;
+using DevsTutorialCenterMVC.Models;
+using DevsTutorialCenterMVC.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevsTutorialCenterMVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Login()
         {
             return View();
@@ -18,5 +28,58 @@ namespace DevsTutorialCenterMVC.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult ResetPassword(string Email, string token)
+        {
+            var viewModel = new ResetPasswordViewModel();
+            if (string.IsNullOrEmpty(token))
+            {
+                ViewBag.ErrToken = "";
+            }
+
+            if (string.IsNullOrEmpty(Email))
+            {
+                ViewBag.ErrEmail = "Email is required";
+            }
+
+            viewModel.Token = token;
+            viewModel.Email = Email;
+
+            return View(viewModel);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError(err.Code, err.Description);
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrEmail = "Email not found";
+                }
+            }
+
+            return View(model);
+        }
+
     }
 }
+
---
+++ DevsTutorialCenterMVC/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevsTutorialCenterMVC.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password mismatch")]
+        public string ConfirmPassword { get; set; }
+    }
+}
