e826eb928f9cea9b9fdca752235eedd36cb198fa
Bentley-Enuvi <bentleyenuvi@gmail.com>
2023-10-31 14:55:21 +0100

    updated

---
+++ DevsTutorialCenterMVC/Models/CommentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DevsTutorialCenterMVC.Models
+{
+    public class CommentsViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string UserId { get; set; }
+        public string ArticleId { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public IEnumerable<CommentsViewModel> Comments { get; set; }
+    }
+}
