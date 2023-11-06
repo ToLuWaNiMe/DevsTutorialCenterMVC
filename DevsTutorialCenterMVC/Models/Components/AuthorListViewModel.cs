ee2873e066f116d09c4bb3c99ad021d3c2c04076
La0lu <oguntoyetobi456@gmail.com>
2023-11-06 14:51:43 +0100

    updated

--- DevsTutorialCenterMVC/Models/Components/AuthorListViewModel.cs
+++ DevsTutorialCenterMVC/Models/Components/AuthorListViewModel.cs
@@ -4,7 +4,7 @@
     {
         public string Image { get; set; }
         public string Name { get; set; }
-        public string Stack { get; set; }
+        public string Designation { get; set; }
         public int NumberOfArticles { get; set; }
 
     }
