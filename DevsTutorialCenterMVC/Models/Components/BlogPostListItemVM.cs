30fdd96fb59645f8a4a299ce0624bfaa4ea2bafc
Marvellous Odemwingie <odemwingiemarv@gmail.com>
2023-11-14 00:13:46 +0100

    added blog post list item component

---
+++ DevsTutorialCenterMVC/Models/Components/BlogPostListItemVM.cs
@@ -0,0 +1,13 @@
+﻿namespace DevsTutorialCenterMVC.Models.Components;
+
+public class BlogPostListItemVM
+{
+    public string Id { get; set; }
+    public string ImageUrl { get; set; }
+    public TagComponentViewModel Tag { get; set; }
+    public string Title { get; set; }
+    public string ReadTime { get; set; }
+    public string Text { get; set; }
+    public AuthorListItemViewModel Author { get; set; }
+    public string CreatedOn { get; set; }
+}
