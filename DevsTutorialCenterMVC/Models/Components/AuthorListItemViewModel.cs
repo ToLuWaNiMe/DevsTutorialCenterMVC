a4a1f601cf6514715dda7642f72c4cabdd3f41e5
Marvellous Odemwingie <odemwingiemarv@gmail.com>
2023-11-13 23:48:42 +0100

    blog post item partial

--- DevsTutorialCenterMVC/Models/Components/AuthorListItemViewModel.cs
+++ DevsTutorialCenterMVC/Models/Components/AuthorListItemViewModel.cs
@@ -1,11 +1,10 @@
 ﻿namespace DevsTutorialCenterMVC.Models.Components
 {
-    public class AuthorListViewModel
+    public class AuthorListItemViewModel
     {
         public string Image { get; set; }
         public string Name { get; set; }
         public string Designation { get; set; }
         public int NumberOfArticles { get; set; }
-
     }
 }
