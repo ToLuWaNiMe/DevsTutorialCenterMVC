f23fe89b9727ca1fd9dbc488f6b22137f10ac4db
La0lu <oguntoyetobi456@gmail.com>
2023-11-20 13:28:55 +0100

    updated

--- DevsTutorialCenterMVC/Models/StoryPageVM.cs
+++ DevsTutorialCenterMVC/Models/StoryPageVM.cs
@@ -7,5 +7,7 @@ namespace DevsTutorialCenterMVC.Models
         public IEnumerable<StoryArticlesVM> PendingArticles { get; set; } = Enumerable.Empty<StoryArticlesVM>();
         public IEnumerable<StoryArticlesVM> PublishedArticles { get; set; } = Enumerable.Empty<StoryArticlesVM>();
         public IEnumerable<StoryArticlesVM> DraftArticles { get; set; } = Enumerable.Empty<StoryArticlesVM>();
+        public IEnumerable<TagViewModel> AllTags { get; set; } = Enumerable.Empty<TagViewModel>();
+        public IEnumerable<AuthorListItemViewModel> TopAuthors { get; set; } = Enumerable.Empty<AuthorListItemViewModel>();
     }
 }
