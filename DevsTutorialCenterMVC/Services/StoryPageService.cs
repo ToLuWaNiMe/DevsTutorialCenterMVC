a349508b1d767c4d47556fde1e7a1c0581ec22ef
La0lu <oguntoyetobi456@gmail.com>
2023-11-20 09:46:36 +0100

    updated

---
+++ DevsTutorialCenterMVC/Services/StoryPageService.cs
@@ -0,0 +1,13 @@
+﻿using DevsTutorialCenterMVC.Services.Interfaces;
+
+namespace DevsTutorialCenterMVC.Services
+{
+    public class StoryPageService : BaseService
+    {
+        public StoryPageService(HttpClient client, IConfiguration config) : base(client, config)
+        {
+        }
+
+
+    }
+}
