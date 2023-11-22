783f6811760151770e814cca2ca316ae6238d325
Marvellous Odemwingie <odemwingiemarv@gmail.com>
2023-11-22 13:12:18 +0100

    changes

--- DevsTutorialCenterMVC/Services/Interfaces/IMessengerService.cs
+++
@@ -1,8 +0,0 @@
-﻿using DevsTutorialCenterMVC.Models;
-
-namespace DevsTutorialCenterMVC.Services.Interfaces;
-
-public interface IMessengerService
-{
-    string Send(Message message, string Attachment = "");
-}
