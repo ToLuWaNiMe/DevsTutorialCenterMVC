d8effe4e009774fd3ab9917007c9403647b0fad1
larrybloom <aniediabasi.joseph@decagon.dev>
2023-11-22 14:57:08 +0100

    integrating123

---
+++ DevsTutorialCenterMVC/Services/TagService.cs
@@ -0,0 +1,35 @@
+﻿using DevsTutorialCenterMVC.Models.Components;
+using DevsTutorialCenterMVC.Models;
+using DevsTutorialCenterMVC.Services.Interfaces;
+
+namespace DevsTutorialCenterMVC.Services
+{
+    public class TagService : BaseService
+    {
+        public TagService(HttpClient client, IHttpContextAccessor httpContextAccessor, IConfiguration config) : base(client, httpContextAccessor, config)
+        {
+        }
+
+
+        public async Task<IEnumerable<TagViewModel>> InterestingTopics()
+        {
+            var address = "/api/tags/get-all-tag";
+            var methodType = "GET";
+
+            var result = await MakeRequest<ResponseObject<IEnumerable<TagViewModel>>, string>(address, methodType, "", "");
+            if (result != null)
+            {
+
+                var mappedResult = result.Data.Select(x => new TagViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                });
+
+                return mappedResult;
+
+            }
+            return Enumerable.Empty<TagViewModel>();
+        }
+    }
+}
